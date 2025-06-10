59c4acf1b3b93c0334121d4ea6480478e6105fb3
Noel Fissahazion <noel.fissahazion@ntnu.no>
2025-06-10 21:40:45 +0200

    test

--- backend/aptitudeapp/views.py
+++ backend/aptitudeapp/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Aptitude Test API is running.")
+
+# This is a placeholder view function that can be replaced with actual logic later.
